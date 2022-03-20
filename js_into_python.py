2f38494b28ad4d036040047b4bfe1ea42ca35966
Iemane291 <xyyl691@gmail.com>
2022-03-20 20:52:08 +0400

    variety of new functions added to the math class, lowercase true and false

--- js_into_python.py
+++ js_into_python.py
@@ -1,4 +1,11 @@
 import math
+import random
+import numpy
+
+from typing import Union
+
+true = True
+false = False
 
 class console:
     def log(*args, **kwargs):
@@ -6,11 +13,40 @@ class console:
 
 
 class Math:
-    def round_(x):
+    def round_(x: Union[int, float]):
         return round(x)
     
-    def sin(x):
+    def sin(x: Union[int, float]):
         return math.sin(x)
     
-    def pow_(x, y):
+    def pow_(x: Union[int, float], y: Union[int, float]):
         return math.pow(x, y)
+    
+    def ceil(x: Union[int, float]):
+        return math.ceil(x)
+
+    def floor(x: Union[int, float]):
+        return math.floor(x)
+    
+    def trunc(x: Union[int, float]):
+        return int(x)
+    
+    def sign(x: Union[int, float]):
+        if x > -1:
+            return True
+        elif x < 0:
+            return False
+        
+    def min_(*args: Union[int, float], **kwargs: Union[int, float]):
+        checkedContainer = list(*args, **kwargs)
+        return min(checkedContainer)
+    
+    def max_(*args: Union[int, float], **kwargs: Union[int, float]):
+        checkedContainer = list(*args, **kwargs)
+        return max(checkedContainer)
+    
+    def random():
+        return random.random()
+    
+    def log(x: Union[int, float]):
+        return numpy.log(x)
